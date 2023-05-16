bfb404487223d9118d1f44766be9164d04c34846
Henrique Nogueira <henriquenogueira@henriques-mbp.home>
2023-05-16 13:38:53 +0100

    setting up users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, NestApplication } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS
   await app.listen(9000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,10 +5,13 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = createUserDto
+    console.log(user.email);
+    
     return this.usersService.create(createUserDto);
   }
 
